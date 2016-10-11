d77efcb6da97df684a1e205a7daecc8fd55b13d7
BitOven <merillas87@gmail.com>
2016-10-11 11:51:15 +0200

    El quesito carga automaticamente, sin necesidad de cargar con boton.

--- app/src/main/java/com/catenaxio/QuesitoActivity.java
+++ app/src/main/java/com/catenaxio/QuesitoActivity.java
@@ -1,6 +1,7 @@
 package com.catenaxio;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -10,7 +11,7 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class QuesitoActivity extends Activity implements View.OnClickListener {
+public class QuesitoActivity extends Activity {
 
     private WebView webView;
     private Button botonCargar;
@@ -27,8 +28,8 @@ public class QuesitoActivity extends Activity implements View.OnClickListener {
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
 
-        botonCargar = (Button) findViewById(R.id.botonRefresh2);
-        botonCargar.setOnClickListener(this);
+//        botonCargar = (Button) findViewById(R.id.botonRefresh2);
+//        botonCargar.setOnClickListener(this);
         //webView.addJavascriptInterface(new WebAppInterface(), "Android");
         //webView.setWebViewClient(new MyWebViewClient());
 
@@ -36,7 +37,7 @@ public class QuesitoActivity extends Activity implements View.OnClickListener {
             Bundle extras = getIntent().getExtras();
             abelG=extras.getInt("AbelG");
             abelD=extras.getInt("AbelD");
-            Log.d("dale", "Abel lleva " + abelD + " goles");
+//            Log.d("dale", "Abel lleva " + abelD + " goles");
             jordan=extras.getInt("Jordan");
             anton=extras.getInt("Anton");
             cano=extras.getInt("Cano");
@@ -47,46 +48,44 @@ public class QuesitoActivity extends Activity implements View.OnClickListener {
             invitado=extras.getInt("Invitado");
             titulo="Goles";
         }
-        webView.loadUrl("file:///android_asset/ejemplo32.html");
         //webView.loadUrl("javascript:drawChart('"+abelG+"','"+abelD+"','"+jordan+"','"+anton+"','"+cano+"','"+meri+"','"+hugo+"','"+juanma+"','"+juan+"','"+invitado+"','"+titulo+"')");
         //contadorr=2;
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v==botonCargar){
-            webView.loadUrl("javascript:drawChart('"+abelG+"','"+abelD+"','"+jordan+"','"+anton+"','"+cano+"','"+meri+"','"+hugo+"','"+juanma+"','"+juan+"','"+invitado+"','"+titulo+"')");
-            /*
-            if(contadorr==-1){
-                webView.loadUrl("javascript:drawChart2()");
-                contadorr--;
-            }
-            else if(contadorr==0){
-                webView.loadUrl("javascript:drawChart()");
-                //webView.loadUrl("javascript:drawChart("+Integer.valueOf(abelG)+","+Integer.valueOf(abelD)+","+Integer.valueOf(jordan)+","+Integer.valueOf(anton)+","+Integer.valueOf(cano)+","+Integer.valueOf(meri)+","+Integer.valueOf(hugo)+","+Integer.valueOf(juanma)+","+Integer.valueOf(juan)+","+Integer.valueOf(invitado)+","+String.valueOf(titulo)+")");
-            }
-            else if(contadorr==1){
-                webView.loadUrl("javascript:drawChart(abelG,abelD,jordan,anton,cano,meri,hugo,juanma,juan,invitado,titulo)");
-                //webView.loadUrl("javascript:drawChart("+String.valueOf(abelG)+","+String.valueOf(abelD)+","+String.valueOf(jordan)+","+String.valueOf(anton)+","+String.valueOf(cano)+","+String.valueOf(meri)+","+String.valueOf(hugo)+","+String.valueOf(juanma)+","+String.valueOf(juan)+","+String.valueOf(invitado)+","+String.valueOf(titulo)+")");
-            }
-            else if(contadorr==2){//funciona !!!!!!!!!!!!!!!
-                webView.loadUrl("javascript:drawChart('"+abelG+"','"+abelD+"','"+jordan+"','"+anton + "','" + cano + "','" + meri + "','" + hugo + "','" + juanma + "','" + juan + "','" + invitado + "','" + titulo + "')");
-            } else if (contadorr ==3){
-                webView.loadUrl("javascript:drawChart(\""+abelG+","+abelD+","+jordan+","+anton+","+cano+","+meri+","+hugo+","+juanma+","+juan+","+invitado+","+titulo+"\")");
-            }
-            else if(contadorr==4){
-                webView.loadUrl("javascript:drawChart(\""+Integer.valueOf(abelG)+","+Integer.valueOf(abelD)+","+Integer.valueOf(jordan)+","+Integer.valueOf(anton)+","+Integer.valueOf(cano)+","+Integer.valueOf(meri)+","+Integer.valueOf(hugo)+","+Integer.valueOf(juanma)+","+Integer.valueOf(juan)+","+Integer.valueOf(invitado)+","+String.valueOf(titulo)+"\")");
-            }
-            else if(contadorr==5){
-                webView.loadUrl("javascript:drawChart(\""+String.valueOf(abelG)+","+String.valueOf(abelD)+","+String.valueOf(jordan)+","+String.valueOf(anton)+","+String.valueOf(cano)+","+String.valueOf(meri)+","+String.valueOf(hugo)+","+String.valueOf(juanma)+","+String.valueOf(juan)+","+String.valueOf(invitado)+","+String.valueOf(titulo)+"\")");
-            }
-            else if(contadorr==6){
-                Toast.makeText(this,"Disponible proximamente", Toast.LENGTH_SHORT).show();
-            }
-            contadorr++;
-            */
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        if(v==botonCargar){
+//            /*
+//            if(contadorr==-1){
+//                webView.loadUrl("javascript:drawChart2()");
+//                contadorr--;
+//            }
+//            else if(contadorr==0){
+//                webView.loadUrl("javascript:drawChart()");
+//                //webView.loadUrl("javascript:drawChart("+Integer.valueOf(abelG)+","+Integer.valueOf(abelD)+","+Integer.valueOf(jordan)+","+Integer.valueOf(anton)+","+Integer.valueOf(cano)+","+Integer.valueOf(meri)+","+Integer.valueOf(hugo)+","+Integer.valueOf(juanma)+","+Integer.valueOf(juan)+","+Integer.valueOf(invitado)+","+String.valueOf(titulo)+")");
+//            }
+//            else if(contadorr==1){
+//                webView.loadUrl("javascript:drawChart(abelG,abelD,jordan,anton,cano,meri,hugo,juanma,juan,invitado,titulo)");
+//                //webView.loadUrl("javascript:drawChart("+String.valueOf(abelG)+","+String.valueOf(abelD)+","+String.valueOf(jordan)+","+String.valueOf(anton)+","+String.valueOf(cano)+","+String.valueOf(meri)+","+String.valueOf(hugo)+","+String.valueOf(juanma)+","+String.valueOf(juan)+","+String.valueOf(invitado)+","+String.valueOf(titulo)+")");
+//            }
+//            else if(contadorr==2){//funciona !!!!!!!!!!!!!!!
+//                webView.loadUrl("javascript:drawChart('"+abelG+"','"+abelD+"','"+jordan+"','"+anton + "','" + cano + "','" + meri + "','" + hugo + "','" + juanma + "','" + juan + "','" + invitado + "','" + titulo + "')");
+//            } else if (contadorr ==3){
+//                webView.loadUrl("javascript:drawChart(\""+abelG+","+abelD+","+jordan+","+anton+","+cano+","+meri+","+hugo+","+juanma+","+juan+","+invitado+","+titulo+"\")");
+//            }
+//            else if(contadorr==4){
+//                webView.loadUrl("javascript:drawChart(\""+Integer.valueOf(abelG)+","+Integer.valueOf(abelD)+","+Integer.valueOf(jordan)+","+Integer.valueOf(anton)+","+Integer.valueOf(cano)+","+Integer.valueOf(meri)+","+Integer.valueOf(hugo)+","+Integer.valueOf(juanma)+","+Integer.valueOf(juan)+","+Integer.valueOf(invitado)+","+String.valueOf(titulo)+"\")");
+//            }
+//            else if(contadorr==5){
+//                webView.loadUrl("javascript:drawChart(\""+String.valueOf(abelG)+","+String.valueOf(abelD)+","+String.valueOf(jordan)+","+String.valueOf(anton)+","+String.valueOf(cano)+","+String.valueOf(meri)+","+String.valueOf(hugo)+","+String.valueOf(juanma)+","+String.valueOf(juan)+","+String.valueOf(invitado)+","+String.valueOf(titulo)+"\")");
+//            }
+//            else if(contadorr==6){
+//                Toast.makeText(this,"Disponible proximamente", Toast.LENGTH_SHORT).show();
+//            }
+//            contadorr++;
+//            */
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,5 +109,28 @@ public class QuesitoActivity extends Activity implements View.OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        webView.loadUrl("file:///android_asset/ejemplo32.html");
+        CargarQuesito cargarQueso = new CargarQuesito();
+        cargarQueso.execute();
+    }
 
+    public class CargarQuesito extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            webView.loadUrl("javascript:drawChart('"+abelG+"','"+abelD+"','"+jordan+"','"+anton+"','"+cano+"','"+meri+"','"+hugo+"','"+juanma+"','"+juan+"','"+invitado+"','"+titulo+"')");
+        }
+    }
 }
